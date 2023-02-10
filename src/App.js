4ec8a45e6dc1a045ef72dcad72a546058dd80f73
Keagan Badenhorst 34209433 <102587925+keagan420@users.noreply.github.com>
2023-02-10 17:36:45 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,25 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import {useState} from 'react';
+import Calendar from 'react-calendar'; 
+import 'react-calendar/dist/Calendar.css'
+import './App.css';
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
+function App() {
+ const [date, setDate] = useState(new Date())
 
-  handleClick = api => e => {
-    e.preventDefault()
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
 
-  render() {
-    const { loading, msg } = this.state
+return (
+ <div className="app">
+   <h1 className="header">React Calendar</h1>
+   <div className="calendar-container">
+     <Calendar onChange={setDate} value={date}/>
+   </div>
+   <div className="text-center">
+      Selected date: {date.toDateString()}
+   </div>
+ </div>
+  )
 
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
-  }
-}
-
-export default App
+export default App;
