f3f8635dafd29bc23b69cffdd76c176d8a47a228
Keagan Badenhorst 34209433 <102587925+keagan420@users.noreply.github.com>
2023-02-10 18:20:05 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import {useState} from 'react';
-import Calendar from 'react-calendar'; 
-import 'react-calendar/dist/Calendar.css'
+import { useState } from 'react';
+import Calendar from 'react-calendar';
 import './App.css';
 
 function App() {
- const [date, setDate] = useState(new Date())
+ const [date, setDate] = useState(new Date());
 
-return (
- <div className="app">
-   <h1 className="header">React Calendar</h1>
-   <div className="calendar-container">
-     <Calendar onChange={setDate} value={date}/>
+ return (
+ <div className='app'>
+   <h1>React Calendar with Range</h1>
+   <div>
+     <Calendar onChange={setDate} value={date} selectRange={true}/>
    </div>
-   <div className="text-center">
-      Selected date: {date.toDateString()}
-   </div>
+   {date.length > 0 ? (
+   <p>
+     <span>Start:</span>{' '} {date[0].toDateString()}
+     &nbsp; to &nbsp;
+     <span>End:</span> {date[1].toDateString()}
+   </p>
+        ) : (
+   <p>
+     <span>Default selected date:</span>{' '} {date.toDateString()}
+   </p>
+        )}
  </div>
-  )
-
+  );
 }
 
 export default App;
